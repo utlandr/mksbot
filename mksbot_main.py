05725129b17f7a3767a3ec68c5fbdc1b7280b6e5
Reed Bell <rbel068@aucklanduni.ac.nz>
2018-11-23 02:18:14 +1300

    Visual reformatting in the \! user command and new amuse cog

--- mksbot_main.py
+++ mksbot_main.py
@@ -6,7 +6,8 @@ import random
 import sys, traceback
 import yaml
 
-from misc_functions import *
+from base_functions import *
+from cogs.amusement_fun import get_random_donger
 
 if not discord.opus.is_loaded():
     discord.opus.load_opus('opus')
@@ -19,7 +20,10 @@ bot = commands.Bot(command_prefix='!', description = config['bot']['description'
 client = discord.Client()
 
 #   Import cogs/extensions
-cogs = ['cogs.reddit']
+cogs = ['cogs.reddit',
+        'cogs.amusement'
+        ]
+        
 if __name__ == '__main__':
     for extension in cogs:
         try:
@@ -41,14 +45,12 @@ bot.remove_command('help')
 @bot.command(pass_context=True)
 async def help(ctx):
     embed = discord.Embed(title="MksBot", description="My commands are:", color=0xeee657)
-
-    embed.add_field(name="!copypasta", value=config['copypasta']['description'], inline=False)
-    embed.add_field(name="!donger", value=get_random_donger(), inline=False)
-    embed.add_field(name="!user", value=config['user']['description'], inline=False)
-
     embed.add_field(name="!info", value="Bot info", inline=False)
     embed.add_field(name="!help", value="Gives this message", inline=False)
-
+    embed.add_field(name="!copypasta", value=config['copypasta']['description'], inline=False)
+    embed.add_field(name="!donger", value=get_random_donger(), inline=False)
+    embed.add_field(name="!roulette <no. to kill> <no. of chambers>", value=config['roulette']['description'], inline = False)
+    embed.add_field(name="!user @<username>", value=config['user']['description'], inline=False)
     await ctx.send(embed=embed)
 
 @bot.command(pass_context=True)
@@ -66,16 +68,6 @@ async def user(ctx, *, member : discord.Member):
 async def user_error(ctx, error):
     if isinstance(error,  commands.BadArgument):
         print(error)
-
-
-@bot.command(pass_context=True)
-async def donger(ctx):
-    await ctx.send(get_random_donger())
-
-
-@bot.command(pass_context=True)
-async def roulette(ctx):
-    await ctx.send(russian_roulette(ctx))
 
 #   Logging options
 logger = logging.getLogger('discord')
