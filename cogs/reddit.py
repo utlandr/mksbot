0f71ffabbb85dacf226aed67a869a396b53efd79
Reed Bell <rbel068@aucklanduni.ac.nz>
2018-11-22 16:16:27 +1300

    Added new cog feature for reddit commands

---
+++ cogs/reddit.py
@@ -0,0 +1,18 @@
+import discord
+import yaml
+from discord.ext import commands
+from cogs.reddit_fun import hot_copypasta
+
+class Reddit:
+    def __init__(self, bot):
+        self.bot = bot
+        self.config = yaml.safe_load(open("config.yml"))
+
+    @commands.command(pass_context=True)
+    async def copypasta(self, ctx):
+        title,content = hot_copypasta(5)
+        pasta_response = discord.Embed(title = title, description = content, color = self.config["copypasta"]["response_colour"])
+        await ctx.send(embed = pasta_response)
+
+def setup(bot):
+    bot.add_cog(Reddit(bot))
