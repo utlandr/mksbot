e8b761813fbc31bc2977df2a9c9b00d2ab3ed0c1
Reed Bell <rbel068@aucklanduni.ac.nz>
2019-01-05 13:34:06 +1300

    Replaced !copypasta with !reddit which: - Is independent of a subreddit (can call any subreddit) - Supports r/all (not technically a subreddit) - Improved searching and response - Improved embedding (Linking and author referall)

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -1,7 +1,7 @@
 import discord
 import yaml
 from discord.ext import commands
-from cogs.reddit_fun import hot_copypasta
+from cogs.reddit_fun import *
 
 #   Reddit cog for all things Reddit
 class Reddit:
@@ -9,13 +9,17 @@ class Reddit:
         self.bot = bot
         self.config = yaml.safe_load(open("config.yml"))
     
-    #   copypasta scrapes hot ocmments from r/copypasta subreddit using PRAW
+    #   copypasta scrapes hot comments from r/copypasta subreddit using PRAW
     @commands.command(pass_context=True)
-    async def copypasta(self, ctx):
-        """Scrape and send a formatted post from the r/copypasta subreddit"""
-        title,content = hot_copypasta(5)
-        pasta_response = discord.Embed(title = title, description = content, color = self.config["copypasta"]["response_colour"])
-        await ctx.send(embed = pasta_response)
+    async def reddit(self, ctx, subreddit="all", sort_by="hot"):
+        """Scrape and send a formatted post from any subreddit"""
+        
+        async with ctx.typing():
+            submission = reddit_post(subreddit,sort_by)
+            reddit_response = reddit_embed(submission)#discord.Embed(title = submission.title, description = submission.selftext, color = self.config["copypasta"]["response_colour"])
+            reddit_response.colour=self.config["reddit"]["response_colour"]
+            #reddit_response.set_author(name= "Submitted By:\t%s"%submission.author, url = submission.shortlink)
+        await ctx.send(embed = reddit_response)
 
 #   This function is used by discord.py to integrate the cog+subroutines into the bot
 def setup(bot):
--- cogs/reddit_fun.py
+++ cogs/reddit_fun.py
@@ -1,30 +1,48 @@
 import praw
 import random
 import yaml
+from discord import Embed
 
-
-def hot_copypasta(n_posts = 10):
-    """Scrapes r/copypasta for material"""
+def reddit_post(sub, sort_by, n_posts = 100):
+    """Scrapes reddit for hot posts"""
     reddit = reddit_instance()
-    subreddit = reddit.subreddit("copypasta")
+    subreddit = reddit.subreddit(sub)
 
-    #   Grab top ten posts in the hot section
-    hot_pasta = subreddit.hot(limit=n_posts)
+    if sub != "all":
+        try:
+            subreddit.id
+        
+        except Exception as e:
+            print("Error: %s"%e)
+            return("r/%s not found"%sub, None)
 
-    #   Return a submission that hasn't previously been shown
-    for submission in hot_pasta:
-        if len(submission.selftext) < 2000:
+    #   Grab a list of non-hidden (unread) posts
+    post_list = subreddit.hot(limit=n_posts)
+
+    #   Return a submission that hasn't previously been shown, hide posts that don't meet embedded criteria
+    for submission in post_list:
+        if (len(submission.selftext)) < 2048 and len(submission.title) < 256 and not submission.stickied:
             submission.hide()
-            return(submission.title,submission.selftext)
+            return(submission)
+
+        else:
+            submission.hide()
+
+def reddit_embed(submission):
+    """Converts a raw submission into a pretty discord embedded message"""
+
+    embedded = Embed(title = submission.title, description = submission.selftext, url=submission.shortlink).set_footer(text="Submitted by:\tu/%s"%submission.author)
+
+    return(embedded)
 
 def reddit_instance():
     """Setup the reddit connection (to bot account)"""
     config = yaml.safe_load(open("config.yml"))
-    reddit = praw.Reddit(   client_id = config["copypasta"]["id"],
-                            client_secret = config["copypasta"]["secret"],
-                            user_agent = config["copypasta"]["user_agent"],
-                            username = config["copypasta"]["username"],
-                            password = config["copypasta"]["pw"]
+    reddit = praw.Reddit(   client_id = config["reddit"]["id"],
+                            client_secret = config["reddit"]["secret"],
+                            user_agent = config["reddit"]["user_agent"],
+                            username = config["reddit"]["username"],
+                            password = config["reddit"]["pw"]
                         )
     return reddit
 
