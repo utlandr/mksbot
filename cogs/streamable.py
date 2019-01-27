8ffbcbe1a01afc0a40afbd4a5b9373cd1b2e31eb
Reed Bell <rbel068@aucklanduni.ac.nz>
2019-01-27 22:43:20 +1300

    Init Commit: Streamable media host
    - Used to host reddit media as streamable has native discord support.

---
+++ cogs/streamable.py
@@ -0,0 +1,32 @@
+import json
+import requests
+import yaml
+
+
+def upload_streamable(video_url, user, pw):
+    """Uploads a video from a url to streamable, returns the streamable url if successful"""
+    api_url = 'https://api.streamable.com/import'
+    headers = {'User-Agent': 'Streamable Discord Upload Bot'}
+    params = {'url': video_url}
+    r = requests.get(api_url,
+                     auth=(user, pw),
+                     headers=headers,
+                     params=params)
+
+    out = json.loads(r.content)
+
+    if out["status"]:
+        return out["shortcode"]
+
+    else:
+        return None
+
+
+def streamable_instance():
+    """Provides details for streamable account instance
+
+    :return: streamable username
+    :return: streamable user password
+    """
+    config = yaml.safe_load(open("config.yml"))
+    return config["streamable"]["user"], config["streamable"]["pw"]
